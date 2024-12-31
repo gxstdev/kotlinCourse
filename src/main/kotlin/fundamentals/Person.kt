fa1e36dd542cf174afa9197900f8cdb8fb0913db
Gabriela <gxstdev@gmail.com>
2024-12-31 19:34:13 -0300

    acessando atributos

--- src/main/kotlin/fundamentals/Person.kt
+++ src/main/kotlin/fundamentals/Person.kt
@@ -1,10 +1,11 @@
 package fundamentals
 
-class Person(var name: String, var age: Int) {
+class Person(var name: String, var age: Int, var address: Address) {
 
     override fun toString(): String {
         return "my name is: ${this.name}. and i'm ${this.age} " +
-                "years old. my age in moths is ${ageInMonths()}."
+                "years old. my age in moths is ${ageInMonths()}." +
+                " my address is $address."
     }
 
     private fun ageInMonths(): Int{
@@ -12,7 +13,21 @@ class Person(var name: String, var age: Int) {
     }
 }
 
+class Address(val street: String, var number: Int){
+    override fun toString(): String {
+        return "Rua $street - NRº $number"
+    }
+}
+
 fun main(){
-    var p = Person("Gabriela", 25)
+    var a = Address("Um", 0)
+    a.number = 344
+
+    var p = Person("Gabriela", 25, a)
     println(p)
+
+    var p2 = Person("Luana", 28, Address("Dois", 145))
+
+    println(p2.address)
+    println(p2.name)
 }
